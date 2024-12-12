520ae9c8bc61218965eb715252711c6bfb5f6106
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-12 13:50:59 +0100

    🐛 Fixed AuthLayout bugs

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import { Paper } from '@mui/material';
-import { PageLayout } from './PageLayout';
+import { Box, Container, Paper } from '@mui/material';
+import Navbar from '../components/Navbar/Navbar';
+import { useNavigation } from '../hooks/useNavigation';
 
 type AuthLayoutProps = {
     children: React.ReactNode;
 };
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+    const { isDrawerOpen, handleDrawerToggle, handleDrawerClose } = useNavigation();
+
     return (
-        <PageLayout maxWidth="xs" centered>
-            <Paper
-                elevation={3}
+        <>
+            <Navbar
+                isDrawerOpen={isDrawerOpen}
+                onDrawerToggle={handleDrawerToggle}
+                onDrawerClose={handleDrawerClose}
+            />
+            <Box
                 sx={{
-                    width: '100%',
-                    padding: 3,
-                    mt: { xs: 2, sm: 8 }
+                    minHeight: '100vh',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                    padding: 2,
                 }}
             >
-                {children}
-            </Paper>
-        </PageLayout>
+                <Container maxWidth="xs">
+                    <Paper
+                        elevation={3}
+                        sx={{
+                            width: '100%',
+                            padding: 3,
+                            mt: { xs: 2, sm: 8 }
+                        }}
+                    >
+                        {children}
+                    </Paper>
+                </Container>
+            </Box>
+        </>
     );
 };
 
-export default AuthLayout
+export default AuthLayout;
